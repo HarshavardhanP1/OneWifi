1f86d69673872543b65bfa6bc88cb88a1332090d
HarshavardhanP1 <Harshavardhan_Pulluru@comcast.com>
2025-03-17 13:27:08 +0530

    Update mon

--- source/stats/wifi_monitor.c
+++ source/stats/wifi_monitor.c
@@ -1068,7 +1068,106 @@ void telemetry_event_akm_count(telemetry_data_t *sta1,int vapindex, char *mac) {
     get_stubs_descriptor()->t2_event_s_fn(telemetry_buff, telemetry_val);
 }
 
-void telemetry_event_wpa3_enhanced(int vapindex, char *mac, int rsnvariant, frame_type_t frame_type, int key_mgmt, wifi_security_modes_t mode, const char *status) {
+#define LOG_LIMIT_PATH "/nvram/log_limit"
+#define DEFAULT_LOG_LIMIT 5
+int get_log_limit()
+{
+    FILE *fp = NULL;
+    int log_limit = DEFAULT_LOG_LIMIT;
+    if (access(LOG_LIMIT_PATH, R_OK) == 0) {
+        fp = fopen(LOG_LIMIT_PATH, "r");
+        if (fp == NULL) {
+            wifi_util_dbg_print(WIFI_MON,"%s:%d: file is empty for log limit \n",__func__, __LINE__);
+            return log_limit;
+        }
+        fscanf(fp, "%d", &log_limit);
+        fclose(fp);
+    }
+    return log_limit;
+}
+
+#define LOG_INTERVAL_PATH "/nvram/log_interval"
+#define DEFAULT_LOG_INTERVAL 5
+int get_log_interval()
+{
+    FILE *fp = NULL;
+    int log_interval = DEFAULT_LOG_INTERVAL;
+    if (access(LOG_INTERVAL_PATH, R_OK) == 0) {
+        fp = fopen(LOG_INTERVAL_PATH, "r");
+        if (fp == NULL) {
+            wifi_util_dbg_print(WIFI_MON,"%s:%d: file is empty for log interval \n",__func__, __LINE__);
+            return log_interval;
+        }
+        fscanf(fp, "%d", &log_interval);
+        fclose(fp);
+    }
+    return log_interval;
+}
+
+int find_message_index(message_data_t *msg_data, const char *message) {
+    for (int i = 0; i < msg_data->msg_count; i++) {
+        if (strcmp(msg_data->messages[i], message) == 0) {
+            return i; // Return the index of the repeated message
+        }
+    }
+    return -1; // Message not found
+}
+
+int rate_limit_log(telemetry_data_t *data, const char *message) {
+    time_t current_time = time(NULL);
+    message_data_t *msg_data = &data->message_data;
+    int index = find_message_index(msg_data, message);
+
+    if (index != -1) {
+        msg_data->repeated_counts[index]++;
+        wifi_util_info_print(WIFI_MON, "Message '%s' found at index %d, repeated count: %d\n", message, index, msg_data->repeated_counts[index]);
+        if (msg_data->repeated_counts[index] > get_log_limit() && difftime(current_time, msg_data->first_set_times[index]) > get_log_interval()) {
+	    wifi_util_info_print(WIFI_MON, "Time difference: %.2f seconds Log rate limit reached for message: %s \n", difftime(current_time, msg_data->first_set_times[index]),message);
+            free(msg_data->messages[index]);
+            for (int i = index; i < msg_data->msg_count - 1; i++) {
+                msg_data->messages[i] = msg_data->messages[i + 1];
+                msg_data->repeated_counts[i] = msg_data->repeated_counts[i + 1];
+                msg_data->first_set_times[i] = msg_data->first_set_times[i + 1];
+            }
+            msg_data->msg_count--;
+            return -1;
+            wifi_util_info_print(WIFI_MON, "Message '%s' reset due to repeated input and time condition.\n", message);
+        }
+	if (msg_data->repeated_counts[index] > 5) {
+	    wifi_util_info_print(WIFI_MON, "Message '%s' skip the log as due to repeated input.\n", message);
+            return -1;
+	}
+        return 0;
+    }
+
+    // Resize the messages array if necessary
+    if (msg_data->msg_count == msg_data->msg_capacity) {
+        msg_data->msg_capacity = msg_data->msg_capacity == 0 ? 1 : msg_data->msg_capacity * 2;
+        msg_data->messages = realloc(msg_data->messages, msg_data->msg_capacity * sizeof(char *));
+        msg_data->repeated_counts = realloc(msg_data->repeated_counts, msg_data->msg_capacity * sizeof(int));
+        msg_data->first_set_times = realloc(msg_data->first_set_times, msg_data->msg_capacity * sizeof(time_t));
+        if (msg_data->messages == NULL || msg_data->repeated_counts == NULL || msg_data->first_set_times == NULL) {
+            wifi_util_info_print(WIFI_MON, "Failed to realloc memory");
+            return -1;
+        }
+        wifi_util_info_print(WIFI_MON, "Resized messages array to capacity %d\n", msg_data->msg_capacity);
+    }
+
+    // Add the new message to the array
+    msg_data->messages[msg_data->msg_count] = malloc((strlen(message) + 1) * sizeof(char));
+    if (msg_data->messages[msg_data->msg_count] == NULL) {
+        wifi_util_info_print(WIFI_MON, "Failed to malloc memory for new message");
+        return -1;
+    }
+    strcpy(msg_data->messages[msg_data->msg_count], message);
+    msg_data->repeated_counts[msg_data->msg_count] = 0;
+    msg_data->first_set_times[msg_data->msg_count] = current_time;
+    msg_data->msg_count++;
+    wifi_util_info_print(WIFI_MON, "Added new message '%s' at index %d\n", message, msg_data->msg_count - 1);
+    return 0;
+}
+
+void telemetry_event_wpa3_enhanced(int vapindex, char *mac, int rsnvariant, frame_type_t frame_type, int key_mgmt, wifi_security_modes_t mode, const char *status, telemetry_data_t *sta) {
     char telemetry_buff[64] = {0};
     char telemetry_val[128] = {0};
     char telemetry_buff_str[64] = {0};
@@ -1087,8 +1186,10 @@ void telemetry_event_wpa3_enhanced(int vapindex, char *mac, int rsnvariant, fram
              "%d,%s,%d,%d,%d,%d,%s", vapindex, mac, rsnvariant, frame_type, key_mgmt, mode, status);
     strncpy(telemetry_buff_str, telemetry_buff, sizeof(telemetry_buff_str) - 1);
     telemetry_buff_str[sizeof(telemetry_buff_str) - 1] = '\0';
-    wifi_util_dbg_print(WIFI_MON, "%s:%s\n", telemetry_buff_str, telemetry_val);
-    get_stubs_descriptor()->t2_event_s_fn(telemetry_buff, telemetry_val);
+    if (rate_limit_log(sta, telemetry_buff_str) == 0) {
+        wifi_util_info_print(WIFI_MON, "%s:%s\n", telemetry_buff_str, telemetry_val);
+        get_stubs_descriptor()->t2_event_s_fn(telemetry_buff, telemetry_val);
+    }
 }
 
 void wpa3_enhanced_connection_akms_count(telemetry_data_t *sta, int expected_akm, int actual_akm) {
@@ -1132,18 +1233,18 @@ int set_sta_client_mode(int ap_index, char *mac, int key_mgmt, frame_type_t fram
     if (frame_type == ASSOC_REQUEST || frame_type == REASSOC_REQUEST) {
         sta->assoc_akm = key_mgmt;
         rsn_variant_t variant = (((int)security->mode == (int)SECURITY_WPA3_Compatibility)?get_rsn_variant(band, key_mgmt):0);
-        telemetry_event_wpa3_enhanced(ap_index, mac, variant, frame_type, key_mgmt, security->mode,"CONNECTION_IN_PROGRESS");
+        telemetry_event_wpa3_enhanced(ap_index, mac, variant, frame_type, key_mgmt, security->mode,"CONNECTION_IN_PROGRESS", sta);
         sta->eapol_akm = 0;
     } else if (frame_type == EAPOL) {
         sta->eapol_akm = key_mgmt;
 	rsn_variant_t variant = (((int)security->mode == (int)SECURITY_WPA3_Compatibility)?get_rsn_variant(band, key_mgmt):0);
         if (sta->assoc_akm == sta->eapol_akm) {
-            telemetry_event_wpa3_enhanced(ap_index, mac, variant, frame_type, key_mgmt, security->mode,"AKM_MATCHED_CONNECTED");
+            telemetry_event_wpa3_enhanced(ap_index, mac, variant, frame_type, key_mgmt, security->mode,"AKM_MATCHED_CONNECTED", sta);
             wifi_util_dbg_print(WIFI_MON, "%s:%d :%d assoc and eapol akms are equal for station found for vap_index:%d station :%s and set the mode:%d eapol_mode:%d assoc_mode:%d band:%d \r\n", __func__, __LINE__, (int)security->mode, ap_index, mac, key_mgmt, sta->eapol_akm, sta->assoc_akm, band);
 	    wpa3_enhanced_connection_akms_count(sta, mode, sta->eapol_akm);
         }
         else {
-            telemetry_event_wpa3_enhanced(ap_index, mac, variant, frame_type, key_mgmt, security->mode,"AKM_MISMATCH_DISCONNECTION");
+            telemetry_event_wpa3_enhanced(ap_index, mac, variant, frame_type, key_mgmt, security->mode,"AKM_MISMATCH_DISCONNECTION", sta);
             wifi_util_dbg_print(WIFI_MON, "%s:%d assoc and eapol akms are not not equal station found for vap_index:%d station :%s and set the mode:%d eapol_mode:%d assoc_mode:%d band:%d \r\n", __func__, __LINE__, ap_index, mac, key_mgmt, sta->eapol_akm, sta->assoc_akm, band);
         }
     }
