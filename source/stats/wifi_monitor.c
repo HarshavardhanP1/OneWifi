43354275bb8a56d76cead24156fc9a4ac64a22e7
HarshavardhanP1 <Harshavardhan_Pulluru@comcast.com>
2025-04-16 12:55:58 +0530

    Update re wifi_monitor.c

--- source/stats/wifi_monitor.c
+++ source/stats/wifi_monitor.c
@@ -1159,21 +1159,19 @@ void process_deauthenticate	(unsigned int ap_index, auth_deauth_dev_t *dev)
     wifi_util_info_print(WIFI_MON, "%s:%d Device:%s deauthenticated on ap:%d with reason : %d\n", __func__, __LINE__, to_sta_key(dev->sta_mac, sta_key), ap_index, dev->reason);
 
     /*Wrong password on private, Xfinity Home and LNF SSIDs*/
-    if ((dev->reason == 2) && ( isVapPrivate(ap_index) || isVapXhs(ap_index) || isVapLnfPsk(ap_index) ) ) {
+
+    /*if ((dev->reason == 2) && ( isVapPrivate(ap_index) || isVapXhs(ap_index) || isVapLnfPsk(ap_index) ) ) {
         get_formatted_time(tmp);
 
         snprintf(buff, 2048, "%s WIFI_PASSWORD_FAIL:%d,%s\n", tmp, ap_index + 1, to_sta_key(dev->sta_mac, sta_key));
-        /* send telemetry of password failure */
         write_to_file(wifi_health_log, buff);
     }
-    /*ARRISXB6-11979 Possible Wrong WPS key on private SSIDs*/
     if ((dev->reason == 2 || dev->reason == 14 || dev->reason == 19) && ( isVapPrivate(ap_index) ))  {
         get_formatted_time(tmp);
 
         snprintf(buff, 2048, "%s WIFI_POSSIBLE_WPS_PSK_FAIL:%d,%s,%d\n", tmp, ap_index + 1, to_sta_key(dev->sta_mac, sta_key), dev->reason);
-        /* send telemetry of WPS failure */
         write_to_file(wifi_health_log, buff);
-    }
+    }*/
     /*Calling process_disconnect as station is disconncetd from vAP*/
     process_disconnect(ap_index, dev);
 }
