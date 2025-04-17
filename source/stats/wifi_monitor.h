05189fd454a33fc449b41acb3315fb33750a539a
HarshavardhanP1 <Harshavardhan_Pulluru@comcast.com>
2025-04-17 13:44:44 +0530

    Update auth sta map wifi_monitor.h

--- source/stats/wifi_monitor.h
+++ source/stats/wifi_monitor.h
@@ -40,6 +40,7 @@ typedef struct {
 typedef struct {
     unsigned char bssid[32];
     hash_map_t *sta_map; //of type sta_data_t
+    hash_map_t *interop_sta_map;
     hash_map_t *wpa3_sta_map;
     struct timespec last_sta_update_time;
     ap_params_t ap_params;
