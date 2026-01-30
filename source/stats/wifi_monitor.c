3f7f59022933215893f49249fb562b4c06b208fd
HarshavardhanP1 <Harshavardhan_Pulluru@comcast.com>
2026-01-30 17:37:20 +0530

    neeew

--- source/stats/wifi_monitor.c
+++ source/stats/wifi_monitor.c
@@ -651,7 +651,7 @@ void telemetry_event_code_count(interop_data_t *sta1, int vapindex, char *mac, c
     bool has_sta_data = has_non_zero_counts(sta1->sta_status_counts, sta1->sta_reason_counts);
     bool has_ap_data  = has_non_zero_counts(sta1->ap_status_counts, sta1->ap_reason_counts);
     ipenable = vap_bss_info->interop_ctrl;
-	wifi_util_dbg_print(WIFI_MON, "%s:%d ipenable:%d \r\n", __func__, __LINE__,vap_bss_info->interop_ctrl);
+	wifi_util_dbg_print(WIFI_MON, "%s:%d ipenable:%d ipenable:%d \r\n", __func__, __LINE__,vap_bss_info->interop_ctrl, ipenable);
     if (!has_sta_data && !has_ap_data && !ipenable) {
         wifi_util_dbg_print(WIFI_MON,
                             "All status and reason counts are zero. Skipping telemetry.\n");
