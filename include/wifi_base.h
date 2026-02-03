3b7bad86a44715d4c23aa37c78c4cd16b6ad7a9c
Rajagopalaswamy M <Rajagopalaswamy_Muthukrishnan@comcast.com>
2026-02-03 12:14:37 +0000

    RFC introduced : xfi_tel_enable_rfc

--- include/wifi_base.h
+++ include/wifi_base.h
@@ -498,6 +498,7 @@ typedef struct {
     bool wpa3_compatibility_enable;
     bool memwraptool_app_rfc;
     bool csi_analytics_enabled_rfc;
+    bool xfi_tel_enable_rfc;
 } wifi_rfc_dml_parameters_t;
 
 typedef struct {
